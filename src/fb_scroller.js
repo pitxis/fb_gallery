a05ac59c9fac9bd1fe29ff94f29db7654611d406
Manuel Peixoto <pitxis2@gmail.com>
2013-07-15 19:58:11 -0300

    Several changes in fb_scroller

--- src/fb_scroller.js
+++ src/fb_scroller.js
@@ -6,7 +6,7 @@
         private_methods = {
 
             //ajax call to retrive images
-            get_fb: function(size, callB, p_url) {
+            get_fb: function(size, callB, p_url, target) {
                 var $this = this,
             	    callback = ((typeof  private_methods[callB] === "function") ? private_methods[callB] : function(){ container.html("<p>Error bad callback!!!</p>"); });
              
@@ -15,7 +15,7 @@
                     dataType: 'Json',
                     success: function (result) {
                 	    private_methods.parse(result, $this.attr("id"));
-                	    callback.call($this, size);
+                	    callback.call($this, size, target);
                     },
                     beforeSend: function () {
                     	// container.html("<p>Wait, facebook processing!!!</p>");
@@ -27,18 +27,37 @@
             },
             
             //first set of images request
-            first_Request: function(size, albumId, callB){
+            first_Request: function(size, albumId, target, callB){
             	var p_url = 'https:' + '//graph.facebook.com/' + albumId + '/photos?limit=' + size;
-			    private_methods.get_fb.call(this, size, "construct", p_url);
+			    private_methods.get_fb.call(this, size, "construct", p_url, target);
             },
              
             //async request for the other images
             start_Interval: function(size, p_url){
         		private_methods.get_fb.call(this, size, "addIcons", p_url);
             },
+
+            parse: function (json, c_id) {
+            
+                var img,
+                    next = ((json.paging && json.paging.next) ? json.paging.next : undefined),
+                    before = ((json.paging && json.paging.previous) ? json.paging.previous : undefined);
+
+                $(json.data).each(function (idx, obj) {
+                    img = {
+                        "icon": obj.picture, 
+                        "pic": obj.source,
+                        "likes": ((obj.likes && obj.likes.data) ? obj.likes.data : undefined),
+                        "name": ((obj.name || "undifined") ? obj.name : undefined)
+                    };
+                    data.push(img);
+                });
+            
+                paging = {'before' : before, 'next': next};
+            },
             
             //html constuct
-            construct: function (size) {
+            construct: function (size, b_pic_id) {
                 var scroller = $("<div class='scroller'>");
                 var img_container = $("<div class='img_container'>");
                 var list = $("<ul class='img_list'>");
@@ -51,80 +70,86 @@
                 size = ((data.length < size) ? data.length : size);
 
                 for(var idx = 0; idx < size; idx++){
-                    var d = $("<img class='prev_icon' idx='" + idx + "' src='" + data[idx].icon + "'>").on("click", 
-                        function(idx){ 
-                            $(".bPic").attr("src", data[$(this).attr("idx")].pic); 
-                        });
-                    list.append($("<li>").append(d));
+                    
+                    var div = $("<img class='prev_icon' idx='" + idx + "' src='" + data[idx].icon + "'>"); 
+
+                    private_methods.bind_icons.call(this, div, data[idx]);   
+                    list.append($("<li>").append(div));
                 }; 
 
                 this.append(scroller);
-                private_methods.bindButtons.call(this);
+                private_methods.bindButtons.call(this, size);
 
                 if (paging["next"]) {
                     private_methods.start_Interval.call(this, size, paging["next"]);
                 }
+
+                var big_pic = $("#" + b_pic_id),
+                    img = data[0];
+ 
+                big_pic.append("<div class='bp_title'><div class='bp_name'>" + img.name + "</div><div class='fb_like'>Login to like</div>");
+                big_pic.append($("<img class='bigPic_img' src='" + img.pic + "' style='max-height:" + this.height() + "px' />"));
+                big_pic.append($(".err", this));
             },
 
-            parse: function (json, c_id) {
-			
-                var img,
-                    next = ((json.paging && json.paging.next) ? json.paging.next : undefined),
-                    before = ((json.paging && json.paging.previous) ? json.paging.previous : undefined);
+            fill_scroller: function(c_idx){
 
-                $(json.data).each(function (idx, obj) {
-                    img = {
-                        "icon": obj.picture, 
-                        "pic": obj.source,
-                    };
-                    data.push(img);
-                });
+                var list = $(".img_list", this).empty();
+
+                for(var idx = c_idx; idx < (c_idx + size); idx++) { 
+                    if (data[idx]) {
+                        
+                        var div = $("<img class='prev_icon' idx='"+idx+"' src='" + data[idx].icon + "'>"); 
+                        
+                        private_methods.bind_icons.call(this, div, data[idx]);  
+                        list.append($("<li>").append(div));
+                    }
+                }
+            },
             
-                paging = {'before' : before, 'next': next};
+            right_click: function(c_idx){
+
+                //c_index += size;
+                private_methods.fill_scroller.call(this, c_idx);
+                private_methods.show_hide_arrows(this, c_idx);
             },
 
-            bindButtons: function() {
+            left_click: function(c_idx){
+
+                //c_index += -(size);
+                private_methods.fill_scroller.call(this, c_idx);
+                private_methods.show_hide_arrows(this, c_idx);
+            },
+
+            open: function(target, data){
+                $("#" + target +"> img").attr("src", data.pic);
+                $(".bp_name","#" + target).html(((data.name) ? data.name : "")) ;
+            },
+
+            bind_icons: function(target, d){
+
+                var container = this,
+                    data = d;
+
+                target.click(function(event){
+                    event.preventDefault();
+                    container.trigger('open', data);
+                });
+            },
+
+            bindButtons: function(size) {
 
     			var container = this;
 
     			$(".left_arrow_vert", container).click(function(event) {
                     event.preventDefault();
-                    container.trigger('before', container);
+                    container.trigger('before', size);
     			});
 
                 $(".right_arrow_vert", container).click(function(event) {
                     event.preventDefault();
-                    container.trigger('next', container);
+                    container.trigger('next', size);
                 });
-            },
-
-		    right_click: function(container, call_side, size){
-
-                c_index += size;
-                private_methods.fill_scroller(container, size);
-                private_methods.show_hide_arrows(container, size);
-		    },
-
-		    left_click: function(container, call_side, size){
-
-				c_index += -(size);
-                private_methods.fill_scroller(container, size);
-                private_methods.show_hide_arrows(container, size);
-		    },
-
-		    fill_scroller:function(container, size){
-
-		    	var list = $(".img_list", container).empty();
-
-                for(var idx = c_index; idx < (c_index + size); idx++) { 
-                    if (data[idx]) {
-                        var d = $("<img class='prev_icon' idx='"+idx+"' src='" + data[idx].icon + "'>").on("click", 
-                            function(idx){ 
-                               $(".bPic").attr("src", data[$(this).attr("idx")].pic); 
-                            });
-                        list.append($("<li>").append(d));
-                    }
-                }
             },
 
             show_hide_arrows: function(container, size){
@@ -174,12 +199,13 @@
                 return $this.each(function(){
                     var $this = $(this);
 
-                    private_methods.first_Request.call($this, settings.size, settings.albumId, "construct");
+                    private_methods.first_Request.call($this, settings.size, settings.albumId, settings.target,"construct");
                     
                     //bind events
                     $this.bind({
-                        'next':   function() { private_methods.right_click($(this), "next", settings.size)},
-                        'before': function() { private_methods.left_click($(this), "before", settings.size)}
+                        'next':   function() { private_methods.right_click.call($this, arguments[1]); },
+                        'before': function() { private_methods.left_click.call($this, arguments[1]); },
+                        'open':   function() { private_methods.open.call($this, settings.target, arguments[1]); }
                     });
                 });
     		},
