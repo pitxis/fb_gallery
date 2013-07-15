ea0dc8f99c98eb76a0cf1b69a33bbb5714a41072
Manuel Peixoto <pitxis2@gmail.com>
2013-07-15 13:37:14 -0300

    add navigation options, and changed the css

--- src/fb_gallery.js
+++ src/fb_gallery.js
@@ -1,13 +1,14 @@
 (function ($) {
     "use strict";
     $.support.cors = true;
-    var log = ((c_log) ? c_log : alert);
-    var paging = {}, data= [],
+    var log = ((c_log) ? c_log : alert), paging = {},
         private_methods = {
 
-            get_fb : function(container, albumId, callB, p_url) {
-            
+            get_fb : function(albumId, user_id, callB, p_url) {
+                
+                var $this = this;
                 p_url = ((p_url === undefined) ? 'https:' + '//graph.facebook.com/' + albumId + '/photos?limit=1' : p_url);
+
                 $.ajax({
                     url: p_url,
                     cache:false,
@@ -15,18 +16,18 @@
                     success: function (result) {
                         if ((result && result.data) && result.data.length > 0) { 
                             var data = private_methods.parse_fb(result);
-                            private_methods[callB](container, data);
-                            container.show();
+                            private_methods[callB].call($this, data);
+                            private_methods.checkLike.call($this, user_id, data.imgs.id, data.imgs.likes);
                         } else {
                             private_methods.error_handler("Sorry, no results found!!!");
                         }
                     },
                     beforeSend: function () {
-                        private_methods.cooltransition(container);
+                        private_methods.cooltransition($this);
                     },
                     error: function(error) {
                        var err = ((error.responseJSON && error.responseJSON.error) ? error.responseJSON.error.message : "");
-                       private_methods.error_handler(container, err);
+                       private_methods.error_handler($this, err);
                     }
                 }); 
             },
@@ -34,7 +35,6 @@
             error_handler : function(container, message){
                 $(".err", container).html("Error while loading new image");
                 log(message);
-                container.show();
             },
 
             parse_fb : function (json) {
@@ -51,29 +51,87 @@
 
                 return {"imgs": img, "paging": {'before' : before, 'next': next}};
             },
-
-            cooltransition: function(container){
-                 container.hide();
-            },
             
-            construct: function (target, data) {
-                var container = $(target), big_pic = $("<div class='bigPic'>"),
+            construct: function (data) {
+
+                var big_pic = $("<div class='bigPic'>"),
                     img = data.imgs;
 
                 paging = data.paging;
+
                 big_pic.append("<div class='bp_right_arrow' idx='0'" + ((data.paging.next) ? "" : "style='display:none'" )  + " ><img src='../assets/arrow.png' /></div>");
                 big_pic.append("<div class='bp_left_arrow' style='display:none' idx='0'><img src='../assets/arrow.png'  /></div>");
-                big_pic.append("<div class='bp_title'><div class='bp_name'>" + img.name + "</div><div class='fb_like'><div class='bt_like' ph_id='" + img.id +"'>Like</div><div class='num_likes'>" + img.likes.length + "</div></div></div>");
-                big_pic.append($("<img class='bigPic_img' src='" + img.pic + "' style='max-width:" + container.width() + "px' />"));
-                big_pic.append($(".err", container));
-                container.html(big_pic);
-                private_methods.bindButtons(container);
+                big_pic.append("<div class='bp_title'><div class='bp_name'>" + img.name + "</div><div class='fb_like'></div>");
+                big_pic.append($("<img class='bigPic_img' src='" + img.pic + "' style='max-width:" + this.width() + "px' />"));
+                big_pic.append($(".err", this));
+                this.html(big_pic);
+                private_methods.bindButtons.call(this);
             },
 
-            bp_arrows: function(container, call_side) {
-                if (paging[call_side]) {
-                    private_methods.get_fb(container, '', "addIconsAndBigPic", paging[call_side]);
+            addIconsAndBigPic: function(data) {
+                
+                var img = data.imgs, $this = this, 
+                    elem_img = $(".bigPic_img", this);
+
+                paging = data.paging;
+                 
+                elem_img.load(function(){
+                    elem_img.animate({
+                        opacity: 1
+                    },500, function() {
+                        $(".bp_name", $this).html( ((img.name) ? img.name : "") );
+                        $(".bt_like", $this).attr("ph_id", img.id);
+                        $(".num_likes", $this).html( ((img.likes) ? img.likes.length : 0) );
+                        
+                        if (paging.next) {
+                            $(".bp_right_arrow", $this).show();
+                        } else {
+                            $(".bp_right_arrow", $this).hide();
+                        }
+
+                        if (paging.before) {
+                            $(".bp_left_arrow", $this).show();
+                        } else {
+                            $(".bp_left_arrow", $this).hide();
+                        }
+                   });
+                });
+                elem_img.attr("src", img.pic);
+               
+                
+            },
+
+            checkLike: function(user_id, img_id, likes){
+
+                var tmp_div = $("<div class='bt_like' ph_id='" + img_id +"'>Like</div><div class='num_likes'>" + ((likes) ? likes.length : 0) + "</div></div>");
+                
+                if(likes){ 
+                    for(var i = 0; i < likes.length; i++){
+                        if(likes[i].id === "431470333617993"){
+                           tmp_div =  $(tmp_div[0]).html("You and "+ likes.length +" already liked").attr("class", "bt_liked");
+                           break;
+                        }
+                    }
                 }
+
+                this.find(".fb_like").html(tmp_div);
+            },
+
+            cooltransition: function(container){
+            },
+
+            bp_arrows: function(call_side, user_id, caller) {
+
+                var bt = caller, $this = this;
+                $this.find("."+bt).hide();
+
+                $(".bigPic_img", this).animate({
+                    opacity: 0.25
+                },500, function() {
+                    if (paging[call_side]) {
+                        private_methods.get_fb.call($this, '', user_id, "addIconsAndBigPic", paging[call_side]);
+                    }
+                });
             },
 
             fb_like: function(access_token){
@@ -88,54 +146,33 @@
                         c_log(r);
                         var numb = $(".num_likes", $this).html();
                         $(".num_likes", $this).html(++numb);
-
                     },
                     error: function(r){
                         c_log(r);
                     }
-
                 });
             },
 
-            bindButtons: function(container) {
-                $(".bp_right_arrow", container).click(function(event) {
+            bindButtons: function() {
+
+                var $this = this;
+
+                $(".bp_right_arrow", $this).click(function(event) {
                     event.preventDefault();
-                    container.trigger('bp_next', container);
+                    $this.trigger('bp_next', "bp_right_arrow");
                 });
 
-                $(".bp_left_arrow", container).click(function(event) {
+                $(".bp_left_arrow", $this).click(function(event) {
                     event.preventDefault();
-                    container.trigger('bp_before', container);
+                    $this.trigger('bp_before', "bp_left_arrow");
                 });
 
-                $(".bt_like", container).click(function(event) {
+                $(".bt_like", $this).click(function(event) {
                     event.preventDefault();
-                    container.trigger('like', container);
+                    $this.trigger('like', $this);
                 });
-            },
-
-            addIconsAndBigPic: function(container, data) {
-                
-                var img = data.imgs;
-                paging = data.paging;
-
-                $(".bigPic_img", container).attr("src", img.pic);
-                $(".bp_name", container).html( ((img.name) ? img.name : "") );
-                $(".bt_like", container).attr("ph_id", img.id);
-                $(".num_likes", container).html( ((img.likes) ? img.likes.length : 0) );
-                
-                if (paging.next) {
-                    $(".bp_right_arrow", container).show();
-                } else {
-                    $(".bp_right_arrow", container).hide();
-                }
-
-                if (paging.before) {
-                    $(".bp_left_arrow", container).show();
-                } else {
-                    $(".bp_left_arrow", container).hide();
-                }
             }
+            
         },
 
         methods = {
@@ -152,14 +189,15 @@
                 }
 
                 return this.each(function(){
-
+                    var $this = $(this);
                     $this.bind({
-                        'bp_next':   function() { private_methods.bp_arrows($(this), "next", 1); },
-                        'bp_before': function() { private_methods.bp_arrows($(this), "before"); },
-                        'like': function() { private_methods.fb_like.call($(this), settings.access_token); }
+                        'bp_next':   function() { private_methods.bp_arrows.call($this, "next", settings.fb_result.userID, arguments[1]); },
+                        'bp_before': function() { private_methods.bp_arrows.call($this, "before", settings.fb_result.userID, arguments[1]); },
+                        'like': function() { private_methods.fb_like.call($this, settings.fb_result.accessToken); }
                     });
-
-                    private_methods.get_fb($this, settings.albumId, "construct");
+                    
+                    c_log(settings.fb_result);
+                    private_methods.get_fb.call($this, settings.albumId, settings.fb_result.userID, "construct");
                 });
             },
 
